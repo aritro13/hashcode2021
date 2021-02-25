eca932fb25cfa37cd877a0228eab633e09f1cfb8
Pratik Panchal <pratik.sp.1112@gmail.com>
2021-02-26 02:29:09 +0530

    7179781

--- pratik/pratik.py
+++ pratik/pratik.py
@@ -15,26 +15,54 @@ files = ['a','b','c','d','e','f']
 
  
 for eachFile in files:
+    output = "<count>\n"
     streetMap = {}
+    
     temp = os.path.join(path, "{}.txt".format(eachFile))
     f = open(temp, "r")
     lines = f.readlines()
     simulationTime, numberOfIntersections, numberOfStreets, numberOfCars, score = map(int, lines[0].split(" "))
 
+    intersections = [0] * numberOfIntersections
+
     for eachLine in lines[1:numberOfStreets+1]:
         t2 = eachLine.split(" ")
-        streetMap[t2[2]] = (int(t2[3]), (int(t2[0]), int(t2[1])))
+        streetMap[t2[2]] = [int(t2[3]), (int(t2[0]), int(t2[1])), 0]
+        intersections[int(t2[1])] += 1
+
         # print("Streets:", eachLine)
-    print(streetMap)
 
     cars = [None] * numberOfCars
     count = 0
     for eachLine in lines[numberOfStreets+1:]:
         cars[count] = eachLine.replace("\n", "").split(" ")[1:]
+        for eachCarStreet in cars[count]:
+            streetMap[eachCarStreet][2] += 1
         # print("Cars:", eachLine)
         count += 1
-    print(cars)
+        
+    # print(streetMap)
+    # print(intersections)
+    # print(cars)
 
-    data = f.read()
+    count = numberOfIntersections
+    for i in range(numberOfIntersections):
+        if intersections[i] > 1:
+            output += "{}\n{}\n".format(i, intersections[i])
+            for eachStreet in streetMap.keys():
+                if streetMap[eachStreet][1][1] == i:
+                    output += "{} {}\n".format(eachStreet, streetMap[eachStreet][2] or 1)
+        else:
+            output += "{}\n{}\n".format(i, intersections[i])
+            for eachStreet in streetMap.keys():
+                if streetMap[eachStreet][1][1] == i:
+                    output += "{} {}\n".format(eachStreet, streetMap[eachStreet][2] or 1)
+    
+    output = output.replace("<count>", str(count))
+    # print(output)
+
+    temp = os.path.join(path, "{}-out.txt".format(eachFile))
+    f = open(temp, "w")
+    f.write(output.strip())
     f.close()
-    print(data)
+    # break
